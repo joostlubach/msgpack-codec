5ce237fc384b41590ee40356e2bd9cfd905bd42a
Joost Lubach <joostlubach@gmail.com>
2023-08-19 11:40:21 +0200

    Extension codec

--- src/extensionCodec.ts
+++ src/extensionCodec.ts
@@ -1,10 +1,4 @@
 import { ExtensionCodec } from '@msgpack/msgpack'
 
 const extensionCodec = new ExtensionCodec()
-
-const DATETIME_TYPE = 0x01
-extensionCodec.register({
-  type: DATETIME_TYPE,
-})
-
 export default extensionCodec
